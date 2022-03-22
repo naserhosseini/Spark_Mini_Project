3b19d1d880406b207a4f7b538c011fa782916b8e
naserhosseini <naser.hosseini@gamil.com>
2022-03-22 08:16:39 -0500

    update the code by adding LIST

--- Spark_Mini_Project.py
+++ Spark_Mini_Project.py
@@ -1,8 +1,8 @@
 from pyspark import SparkContext,SparkConf
 from operator import add
 
+
 def extract_make_key_value(vector):
-    rdd = 0
     if vector[0] == 'A':
         rdd = (vector[1], vector[2]), 1
     else:
@@ -10,27 +10,16 @@ def extract_make_key_value(vector):
     return rdd
 
 
-def populate_make(line):
-    i = 0
-    make=''
-    model=''
-    x=[]
+def populate_make(entry):
+    x = []
+    line = list(entry)
     for n in line:
-        for m in n:
-            i += 1
-            if i==1:
-                s=m
-            if i == 2:
-                if len(m)>0:
-                    make = m
-            if i==3:
-                if len(m)>0:
-                    model= m
-                    x.append((s,make,model))
-                else:
-                    x.append((s,make,model))
+        s = n[0]
+        if len(n[1]) > 0:
+            make, model = n[1], n[2]
+            x.append((s, make, model))
         else:
-            i = 0
+            x.append((s, make, model))
     return x
 
 
@@ -38,8 +27,9 @@ def extract_vin_key_value(line):
     sr = line.split(',')
     return (sr[2]), (sr[1], sr[3], sr[5]),
 
-conf=SparkConf().setAppName('InnoWatts').setMaster('local')
-sc=SparkContext(conf=conf)
+
+conf = SparkConf().setAppName('InnoWatts').setMaster('local')
+sc = SparkContext(conf=conf)
 
 raw_rdd = sc.textFile('data.csv')
 vin_kv = raw_rdd.map(lambda x: extract_vin_key_value(x))
@@ -48,9 +38,9 @@ vin_kv.collect()
 enhance_make = vin_kv.groupByKey().flatMap(lambda kv: populate_make(kv[1]))
 
 make_kv = enhance_make.map(lambda x: extract_make_key_value(x))
-make_kv=make_kv.filter(lambda x: x is not None)
+make_kv = make_kv.filter(lambda x: x is not None)
 make_kv.collect()
 
-accident=make_kv.reduceByKey(add)
+accident = make_kv.reduceByKey(add)
 print(accident.collect())
 
